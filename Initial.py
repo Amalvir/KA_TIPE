44cc2199bda300bf588bc7b14b2028bfd380713b
Amalvir <samalonsovirissel@gmail.com>
2019-07-02 19:42:04 +0200

    Marche sans animation Ajout de commentaire pour la clarté

--- Initial.py
+++ Initial.py
@@ -1,43 +1,19 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patch
 import numpy as np
-from celluloid import Camera
-# import Volume as V
 
-## Objectifs:
-# Obtenir la position, et le volume immergé en fonction de l'angle téta
-
-# Obtenir le MSIT en fonction des positions relatives des centres de carène et de gravité et du Volume:
-""" On modélise notre engin flottant par un pavé de profondeur b, de hauteur h, de largeur l """
 
 h = 9.5
-# b=
 l = 34
 robj = 0.8*10**3   # Masse volumique de l'objet en kg/m^3
 rofl = 10**3   # Masse volumique du fluide kg/m^3
-# Vp est le volume du pavé
-# Vp=l*b*h
-# I est le moment quadratique
-# H rayon du cercle que forme les centres de carène en fonction de téta
-# i hauteur immergée de l'objet d'après archimède:
-i = robj/rofl*h
-# V=i*l*b  #V volume de la partie imergée
-# I=b*h**3/12 
-# H=I/V
-# a=  #a distance entre G et C le centre de carène ce qu'est relou à calculer
-# MSIT=rofl*(I-V*a) 
-# 
-# def affichage_de_la_situation_initiale3D(teta):
-#     X=[0,l,l,0,0,l,l,0]
-#     Y=[0,0,b,b,0,0,b,b]
-#     Z=[0,0,0,0,h,h,h,h]
-#     
+i = robj/rofl*h   # i hauteur immergée de l'objet d'après archimède:
 
 
 #On reste dans le plan
-def affichage_la_situation_initiale2D():
+def init():
     """Plot les conditions initiales"""
-    # X = [-l/2, l/2, l/2, -l/2, -l/2]  #ABCDA
-    # Z = [h - i, h - i, -i, -i, h - i]
+
     X, Z = rotation(0)
     EAU = [-2*l, 2*l]
     NIV_EAU = [0, 0]
@@ -54,19 +30,12 @@ def affichage_la_situation_initiale2D():
 
 
 def rotation(teta):
-    """Renvoie la liste des X et Z pour plot la rotation"""
+    """Renvoie les listes des X et Z des points ABCD du rectangle ayant fait une rotation teta"""
     rDC = ((l/2)**2 + (h-i)**2)**(1/2)
     rAB = (i**2 + (l/2)**2)**(1/2)
 
     phiDC = np.arctan((h - i)/(l/2))
-    # print("phiDC", phiDC)
     phiAB = np.arctan((i/(l/2)))
-    # print("phiAB", phiAB)
-
-    # C0 = rDC*np.exp(1j*phiDC)
-    # D0 = rDC*np.exp(1j*(-phiDC+np.pi))
-    # B0 = rAB*np.exp(1j*(-phiAB))
-    # A0 = rAB*np.exp(1j*(np.pi + phiAB))
 
     Cj = rDC*np.exp(1j*(phiDC + teta))
     Dj = rDC*np.exp(1j*(np.pi - phiDC + teta))
@@ -77,79 +46,49 @@ def rotation(teta):
 
 
 def racines(teta):
-    """Renvoie les points de constacts avec l'eau en fonction de teta"""
+    """Renvoie les points de contacts avec l'eau en fonction de teta"""
 
     sol = []
-    # On récupère les coords des points
+    # On récupère les coordonnés des points
     X, Y = rotation(teta)
-    for i in range(len(Y) - 1):
-        if Y[i]*Y[i+1] < 0:
-            coeff = (Y[i+1] - Y[i])/(X[i+1] - X[i])
-            A = np.array([[coeff, 1], [0, 1]])
-            B = np.array([0, Y[i] - coeff*X[i]])
-            S = np.linalg.solve(A, B)
-            sol.append(S[0])
 
-    # # systeme d'équation
-    # 0 = np.tan(teta)*x + b
-    # Y[0] - tan(teta)*X[0] = b
+    for j in range(len(Y) - 1):
+        if Y[j]*Y[j+1] < 0:
+            # On test si les coordonnées Y sont 2 à 2 de même signes
+            # Calcul du coeff directeur de la droite
+            coeff = (Y[j+1] - Y[j])/(X[j+1] - X[j])
 
-    # Matrices :
-    # A = np.array([[np.tan(teta), 1], [0, 1]])
-    # B = np.array([0, Y[0] - np.tan(teta)*X[0]])
-    # S = np.linalg.solve(A, B)
-    # S = [x, b] coordonné du point ou ca touche l'eau : [x, 0]
-    # print(sol)
+            # Système d'équations : (inconnues : x, b)
+            # coeff*x + b = 0
+            # 0*x + b = Y[j] - coeff*X[j]
+            A = np.array([[coeff, 1], [0, 1]])      # Matrices des variables
+            B = np.array([0, Y[j] - coeff*X[j]])    # Matrices des constantes
+            S = np.linalg.solve(A, B)   # Pivot de Gauss
+            sol.append(S[0])    # On a besoin que de x donc on append que S[0]
     return sol
 
 
 def points(teta):
-    """Affiche les points ABCD en fonction de teta"""
+    """Affiche les points ABCD ainsi que leurs noms en fonction de teta"""
     X, Z = rotation(teta)
     P = ['C', 'D', 'A', 'B']
-    for i in range(len(P)):
-        plt.plot(X[i], Z[i], 'ob')
-        plt.annotate(P[i], xy=(X[i], Z[i]))
+    for j in range(len(P)):
+        plt.plot(X[j], Z[j], 'ob')
+        plt.annotate(P[j], xy=(X[j], Z[j]))
 
 
 def affichage(teta):
-    fig = plt.figure(figsize=[7, 7])
-    camera = Camera(fig)
+    plt.figure(figsize=[7, 7])
     plt.axis([-20, 20, -20, 20])
-    affichage_la_situation_initiale2D()
+    init()
+    X, Z = rotation(teta)
+    plt.plot(X, Z)
+    points(teta)
+    root = racines(teta)
+    for i in root:
+        plt.plot(i, [0], 'o', color="orange")
 
-    camera.snap()
-    if isinstance(teta, list) or isinstance(teta, np.ndarray):
-        affichage_la_situation_initiale2D()
-        for agl in teta:
-            X, Z = rotation(agl)
-            plt.plot(X, Z)
-            points(agl)
-            root = racines(agl)
-            for j in root:
-                plt.plot(j, [0], 'o', color="orange")
-            camera.snap()
-
-        anim = camera.animate()
-        plt.show()
-    # else:
-    #     affichage([teta])
+    plt.show()
 
 
-
-    # X, Z = rotation(teta)
-    # plt.plot(X, Z)
-    # points(teta)
-    # root = racines(teta)
-    # for i in root:
-    #     plt.plot(i, [0], 'o', color="orange")
-
-    # affichage_la_situation_initiale2D()
-    # On décide de tourner autour du point O, projeté de G sur NIV_EAU
-    # plt.savefig("Magnifique.pdf")
-
-
-L = np.linspace(0, np.pi/6, 50)
-
-
-affichage(L)
+affichage(np.pi/6)
