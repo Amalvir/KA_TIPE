bc31fda4ad278cecfbfca080b5de63a78d8d5096
Amalvir <samalonsovirissel@gmail.com>
2019-10-29 12:44:30 +0100

    LALA

--- Initial.py
+++ Initial.py
@@ -2,7 +2,7 @@
 
 from Affichage import * # pylint: disable=unused-wildcard-import
 
-affichage(np.linspace(0, 2*np.pi, 500))
+affichage(np.linspace(0, 2*np.pi, 2000))
 # affichage(np.pi/40)
 
 # L = tri(rotation(np.pi/40))
